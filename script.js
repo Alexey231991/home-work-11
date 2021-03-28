539e71eebbe1355b9c62efac206be8ce7d85aa63
Alexey231991 <lorketalex@ukr.net>
2021-03-28 14:12:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const colors = ['blue', 'lime', 'yellow']
 
 
-for (let key = 0; key < 
-document.querySelectorAll('.box').length; key++) 
-document.querySelectorAll('.box')
-[key].addEventListener('click', () => 
-document.querySelectorAll('.box')
-[key].style.backgroundColor = 
-colors[document.querySelectorAll('.box')
-[key].innerHTML++ % colors.length])
+const boxes = document.querySelectorAll('.box')
+
+for (let key = 0; key < boxes.length; key++) {
+    const box = boxes[key]
+
+    let count = 0
+    
+    box.addEventListener('click', function () {
+        const position = count % colors.length
+
+        box.style.backgroundColor = colors[position]
+
+        count++
+        box.innerHTML = count
+    })
+}
